931651a7f594e5a8101f043d9511d7eae1c9f25a
Sanskriti Negi <sanskritinegi@Sanskritis-iMac.local>
2023-09-18 06:47:11 -0600

    json for home added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Home from './Pages/Home';
 import Games from './Pages/Games';
 import {HashRouter, Routes, Route, BrowserRouter} from 'react-router-dom';
 
+import homeData from './JsonFiles/homeData';
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path = "/" element = {<div><Home/></div>}/>
+        <Route path = "/" element = {<div><Home values = {homeData}/></div>}/>
         <Route path = "/games" element = {<div><Games/></div>}/>
       </Routes>
       
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,6 @@
 import Card from "../Components/Card";
 
-function Home(){
+function Home(props){
     return (
         <main className="page">
             <div className="page-title">
@@ -10,23 +10,23 @@ function Home(){
             <div className="full-bar">
                 <Card
                     bgColor = "pink"
-                    title = "sdfsdf"
-                    description = "dsfsdf"
-                    link = "/games"
+                    title = {props.values[0]? props.values[0].title: ""}
+                    description = {props.values[0]? props.values[0].description: ""}
+                    link = {props.values[0]? props.values[0].link: ""}
                 />
             </div>
             <div className="split-bar">
                 <Card
                     bgColor = "yellow"
-                    title = "sdfsdf"
-                    description = "dsfsdf"
-                    link = "https://www.google.com/"
+                    title = {props.values[1]? props.values[1].title: ""}
+                    description = {props.values[1]? props.values[1].description: ""}
+                    link = {props.values[1]? props.values[1].link: ""}
                 />
                 <Card
                     bgColor = "pink"
-                    title = "sdfsdf"
-                    description = "dsfsdf"
-                    link = "https://www.google.com/"
+                    title = {props.values[2]? props.values[2].title: ""}
+                    description = {props.values[2]? props.values[2].description: ""}
+                    link = {props.values[2]? props.values[2].link: ""}
                 />
             </div>
             <div className="wide-section">
@@ -34,23 +34,23 @@ function Home(){
                     <div className="split-row-bar">
                         <Card
                             bgColor = "yellow"
-                            title = "sdfsdf"
-                            description = "dsfsdf"
-                            link = "https://www.google.com/"
+                            title = {props.values[3]? props.values[3].title: ""}
+                    description = {props.values[3]? props.values[3].description: ""}
+                    link = {props.values[3]? props.values[3].link: ""}
                         />
                         <Card
                             bgColor = "pink"
-                            title = "sdfsdf"
-                            description = "dsfsdf"
-                            link = "https://www.google.com/"
+                            title = {props.values[4]? props.values[4].title: ""}
+                            description = {props.values[4]? props.values[4].description: ""}
+                            link = {props.values[4]? props.values[4].link: ""}
                         />
                     </div>
                     <div className="full-row-bar">
                             <Card
                                 bgColor = "yellow"
-                                title = "sdfsdf"
-                                description = "dsfsdf"
-                                link = "https://www.google.com/"
+                                title = {props.values[5]? props.values[5].title: ""}
+                                description = {props.values[5]? props.values[5].description: ""}
+                                link = {props.values[5]? props.values[5].link: ""}
                             />
                     </div>
                 </div>
