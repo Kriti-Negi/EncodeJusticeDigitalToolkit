0214915823ab3a2fb6de7a8d02023509859b6d58
gracesilge <42701781+gracesilge@users.noreply.github.com>
2023-10-17 12:24:34 -0600

    Create biasData.js

---
+++ src/JsonFiles/biasData.js
@@ -0,0 +1,37 @@
+let biasData = [];
+
+biasData = [
+        {
+            title: "Some Placeholder values ",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.",
+            link: "https://www.google.com/"
+        },
+        {
+            title: "Some Placeholder values ",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.",
+            link: "https://www.google.com/"
+        },
+        {
+            title: "Some Placeholder values ",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.",
+            link: "https://www.google.com/"
+        },
+        {
+            title: "Some Placeholder values ",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.",
+            link: "https://www.google.com/"
+        },
+        {
+            title: "Some Placeholder values ",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.",
+            link: "https://www.google.com/"
+        },
+        {
+            title: "Some Placeholder values ",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.",
+            link: "https://www.google.com/"
+        }
+    ]
+
+
+export default biasData;
