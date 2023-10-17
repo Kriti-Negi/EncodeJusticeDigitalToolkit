d5917ba1340c4da77e65e65069e899d16bc7a158
gracesilge <42701781+gracesilge@users.noreply.github.com>
2023-10-17 13:12:53 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import './App.css';
 import Home from './Pages/Home';
-import Games from './Pages/Games';
+import Games from './Pages/Games'
+import Art from './Pages/Art';
+import Bias from './Pages/Bias';
+import Binary from './Pages/Binary';
+import Career from './Pages/Career';
+import Zine from './Pages/Zine';
 import {HashRouter, Routes, Route, BrowserRouter} from 'react-router-dom';
 
 import homeData from './JsonFiles/homeData';
 import gameData from './JsonFiles/gameData';
+import artData from './JsonFiles/artData';
+import biasData from './JsonFiles/biasData';
+import binaryData from './JsonFiles/binaryData';
+import careerData from './JsonFiles/careerData';
+import zineData from './JsonFiles/zineData';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path = "/" element = {<div><Home values = {homeData}/></div>}/>
         <Route path = "/games" element = {<div><Games values = {gameData}/></div>}/>
+        <Route path = "/art" element = {<div><Games values = {artData}/></div>}/>
+        <Route path = "/AIBias" element = {<div><Games values = {biasData}/></div>}/>
+        <Route path = "/binaryInformation" element = {<div><Games values = {binaryData}/></div>}/>
+        <Route path = "/CScareers" element = {<div><Games values = {careerData}/></div>}/>
+        <Route path = "/zines" element = {<div><Games values = {zineData}/></div>}/>
       </Routes>
       
     </BrowserRouter>
