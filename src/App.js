e5e06fb90a2548bac295f4a502cfef934447b772
Sanskriti Negi <sanskritinegi@Sanskritis-iMac.local>
2023-10-30 21:42:47 -0600

    :

--- src/App.js
+++ src/App.js
@@ -18,15 +18,15 @@ import zineData from './JsonFiles/zineData';
 function App() {
   return (
     <HashRouter>
-      <div>
-        <Route path = "/" element = {<div><Home values = {homeData}/></div>}/>
+      <Routes>
+        <Route exact path = "/" element = {<div><Home values = {homeData}/></div>}/>
         <Route path = "./games" element = {<div><Games values = {gameData}/></div>}/>
-        <Route path = "/art" element = {<div><Games values = {artData}/></div>}/>
-        <Route path = "/AIBias" element = {<div><Games values = {biasData}/></div>}/>
-        <Route path = "/binaryInformation" element = {<div><Games values = {binaryData}/></div>}/>
-        <Route path = "/CScareers" element = {<div><Games values = {careerData}/></div>}/>
-        <Route path = "/zines" element = {<div><Games values = {zineData}/></div>}/>
-      </div>
+        <Route path = "./art" element = {<div><Games values = {artData}/></div>}/>
+        <Route path = "./AIBias" element = {<div><Games values = {biasData}/></div>}/>
+        <Route path = "./binaryInformation" element = {<div><Games values = {binaryData}/></div>}/>
+        <Route path = "./CScareers" element = {<div><Games values = {careerData}/></div>}/>
+        <Route path = "./zines" element = {<div><Games values = {zineData}/></div>}/>
+      </Routes>
       
     </HashRouter>
     
